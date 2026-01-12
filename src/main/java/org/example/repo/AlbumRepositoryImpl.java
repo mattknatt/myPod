32c31b4315722fda9e9e57262e5eb123df8b2742
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-01-12 11:32:52 +0100

    Changes by mattiashagstrom

--- src/main/java/org/example/repo/AlbumRepositoryImpl.java
+++ src/main/java/org/example/repo/AlbumRepositoryImpl.java
@@ -11,7 +11,7 @@ public class AlbumRepositoryImpl implements AlbumRepository {
 
     private final EntityManagerFactory emf;
 
-    public AlbumRepositoryImpl(EntityManagerFactory emf) {
+    public AlbumRepositoryImpl (EntityManagerFactory emf) {
         this.emf = emf;
     }
 
