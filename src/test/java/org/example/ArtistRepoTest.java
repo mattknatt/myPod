497baa05c8c070369dc4862bd6f5f22e84a01aec
mattknatt <mattiashagstrommusic@gmail.com>
2026-01-13 15:46:36 +0100

    First ArtistRepoTest tests written.
    Co-authored-by: Simon Forsberg <simon.co.forsberg@gmail.com>

---
+++ src/test/java/org/example/ArtistRepoTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.example.entity.Artist;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArtistRepoTest extends RepoTest {
+
+    @Test
+    void existsByUniqueId_shouldFindSpecificArtistIfPresent() {
+        // Given, When
+        boolean artistExists = artistRepo.existsByUniqueId(testArtist1);
+
+        // Then
+        assertThat(artistExists).isTrue();
+    }
+
+    @Test
+    void save_shouldSaveNewArtist() {
+        // Given
+        Artist testArtist3 = new Artist(12L,"Test Gärdestad", "Sweden");
+
+        // When
+        artistRepo.save(testArtist3);
+        List<Artist> artists = artistRepo.findAll();
+
+        // Then
+        assertThat(artists.contains(testArtist3));
+    }
+
+    @Test
+    void findAll_shouldFindArtists() {
+        // Given, When
+
+
+
+        // Then
+    }
+}
